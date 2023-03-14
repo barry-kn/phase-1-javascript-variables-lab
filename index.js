166add4284100e67dd40a825bd4f0f7574a00f39
barry-kn <carrybarryklein49@gmail.com>
2023-03-14 11:54:05 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+//let companyName = 'Scuber';
+const companyName ='Scuber'
+//mostProfitableNeighborhood=='Chelsea'
+//var mostProfitableNeighborhood = 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea'
+//var companyCeo = 'Susan Smith'
+let companyCeo = 'Susan Smith'
+
